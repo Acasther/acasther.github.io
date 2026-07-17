c6ff1de920fccf39d9d52a63ac22b3520de4fc52
Andres Castrillo <acastrilloh@gmail.com>
2026-07-17 13:25:29 -0600

    changed phone number

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const PHONE_NUMBER = '+506 8390-8070';
+export const PHONE_NUMBER = '+506 8844-4418';
 export const EMAIL_ADDRESS = 'info@costalegalcr.com';
 export const OFFICE_LOCATION = 'Escazú, San José';
 export const OFFICE_LOCATION_LONG = 'Edificio Monterrico, Trejos Montealegre, Escazú';
